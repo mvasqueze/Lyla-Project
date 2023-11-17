f15a9fa76b3df36dffd17aa08ea7c132fd473eca
mvasqueze <mvasqueze@eafit.edu.co>
2023-11-17 18:20:45 -0500

    Graficadora funcional

--- LylaProjectPro/graficador/urls.py
+++ LylaProjectPro/graficador/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.graficadorSimple, name='graficador'),
+    path('', views.graficadorSimple, name='graficadorSimple'),
     path('<path:funcion>', views.graficador, name='graficador'),
 ]
--- LylaProjectPro/graficador/views.py
+++ LylaProjectPro/graficador/views.py
@@ -45,25 +45,24 @@ def graficadorSimple(request):
                 error = f"Error al evaluar la función: {e}"
                 print(error)
 
-    return render(request, 'graficador.html', {'imagen_grafico': imagen_grafico, 'error': error})
+    return render(request, 'graficadorSimple.html', {'imagen_grafico': imagen_grafico, 'error': error})
 
 def graficador(request, funcion):
     print('esta es la función: ')
-    funcion1 = request.GET.get('funcion')
-    print(funcion1)
+    print(funcion)
     
     imagen_grafico = None
     error = None
     
     # Graficar la función
-    if funcion1:
+    if funcion:
         try:
             x = np.linspace(-10, 10, 1000) 
             y = np.empty_like(x)
             # Evaluate the expression for each x value
             for i, x_val in enumerate(x):
                 # Use eval with a dictionary to allow math functions within the eval context
-                y[i] = eval(funcion1, {'__builtins__': None}, {'x': x_val, 'math': math})
+                y[i] = eval(funcion, {'__builtins__': None}, {'x': x_val, 'math': math})
             # Evaluate the function to generate y values
             print('B: ')
             print(y)
@@ -72,7 +71,7 @@ def graficador(request, funcion):
             plt.plot(x, y)
             plt.xlabel('x')
             plt.ylabel('f(x)')
-            plt.title('Gráfico de la función: ' + funcion1)
+            plt.title('Gráfico de la función: ' + funcion)
             plt.grid(True)
 
             # Convert the plot to an image to display on the page
@@ -87,4 +86,4 @@ def graficador(request, funcion):
             error = f"Error al evaluar la función: {e}"
             print(error)
 
-    return render(request, 'graficador.html', {'imagen_grafico': imagen_grafico, 'error': error, 'funcion1':funcion1})
+    return render(request, 'graficador.html', {'imagen_grafico': imagen_grafico, 'error': error, 'funcion':funcion})
